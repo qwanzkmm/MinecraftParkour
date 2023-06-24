a10f9ce065c9be86f95b55ee973cdb6fdd463194
DIImoNn22 <112386300+DIImoNn22@users.noreply.github.com>
2023-06-24 13:43:22 -0600

    New player controller version

--- Assets/Plugins/YandexSDKControllerCS.cs
+++ Assets/Plugins/YandexSDKControllerCS.cs
@@ -1,8 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class YandexSDKControllerCS : MonoBehaviour
 {
+    public static YandexSDKControllerCS instance;
+    [DllImport("__Internal")] private static extern void GetTypePlayformDevice();
+
     
+    public enum DeviceTypeWeb
+    {
+        Mobile,
+        Desktop
+    }
+    
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+    
+    private void Start()
+    {
+        GetPlatformDevice();
+    }
+    
+    public DeviceTypeWeb CurrentDeviceType { get; private set; }// = DeviceTypeWEB.Mobile;
+
+    public void SetTargetDeviceType(string typeDevice)
+    {
+        switch (typeDevice)
+        {
+            case "desktop": CurrentDeviceType = DeviceTypeWeb.Desktop; break;
+            case "mobile": CurrentDeviceType = DeviceTypeWeb.Mobile; break;
+
+            default: CurrentDeviceType = DeviceTypeWeb.Mobile; break;
+        }
+    }
+    
+
+    public void GetPlatformDevice() => GetTypePlayformDevice();
 }
---
+++ Assets/Plugins/YandexSDKControllerJS.jslib
@@ -0,0 +1,22 @@
+mergeInto(LibraryManager.library, {
+
+  GetTypePlayformDevice: function() {
+    getTypeDevice();
+  },
+
+  SetToLeaderboard: function(value) {
+    ysdk.getLeaderboards()
+      .then(lb => {
+        lb.setLeaderboardScore('Killer', value);
+      });
+  },
+
+  GetLang : function(){
+    var lang = ysdk.environment.i18n.lang;
+    var bufferSize = lengthBytesUTF8(lang) + 1;
+    var buffer = _malloc(bufferSize);
+    stringToUTF8(lang, buffer, bufferSize);
+    return buffer;
+  },
+
+});
